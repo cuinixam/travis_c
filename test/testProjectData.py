57182f830a69bd5d5312d64565d165366f03c4d2
alexandru.maxiniuc <alexandru.maxiniuc@gmail.com>
2016-12-10 21:55:57 +0100

    [OK] Find all files in project

--- test/testProjectData.py
+++ test/testProjectData.py
@@ -1,6 +1,13 @@
 import unittest
 
-import ProjectData
+from ProjectData import ProjectData
+
+import os
+import sys
+currentFileAbsPath = os.path.dirname(os.path.realpath(sys.argv[0]))
+oneFileProjectPath = currentFileAbsPath + '/data/projects/one_file'
+simpleProjectPath  = currentFileAbsPath + '/data/projects/simple'
+moreFoldersProjectPath  = currentFileAbsPath + '/data/projects/more_folders'
 
 class ProductiveCode(unittest.TestCase):
     """
@@ -9,3 +16,54 @@ class ProductiveCode(unittest.TestCase):
     """
     def test_ClassExists(self):
         self.assertTrue(ProjectData, 'The class ProjectData should exist')
+    
+    def test_RootFolderArgument(self):
+        # Invalid root folder
+        my_project_data = ProjectData(root_folder='pa96htgex4dzhkGJ')
+        self.assertTrue(my_project_data, 'The ProjectData object should be created')
+        self.assertFalse(my_project_data.rootFolderAbsPath,'Root folder must exist!')
+        self.assertEqual(my_project_data.productiveCodeNoFiles, 0, 'Default: no files found')
+        # Valid root folder
+        my_project_data = ProjectData(root_folder=oneFileProjectPath)
+        self.assertTrue(my_project_data.rootFolderAbsPath,'Root folder exists!')
+        
+    def test_OneProjectFile(self):
+        my_project_data = ProjectData(root_folder=oneFileProjectPath)
+        self.assertEqual(my_project_data.productiveCodeNoFiles, 1, 'One file was found')
+        self.assertEqual(my_project_data.productiveCodeFiles, ["main.c"], 'The file  main.c should be found!')
+    
+    def test_SimpleProject(self):
+        my_project_data = ProjectData(root_folder=simpleProjectPath)
+        self.assertEqual(my_project_data.productiveCodeNoFiles, 3, 'All files must be found')
+        self.assertEqual(len(my_project_data.productiveCodeFiles),
+                         my_project_data.productiveCodeNoFiles,
+                         'The number of files should correspond to the file list length')
+        self.assertEqual(sorted(my_project_data.productiveCodeFiles),
+                         sorted(["main.c","module.h","module.c"]),
+                         'All files should be found')
+        
+    def test_MoreFoldersProject(self):
+        my_project_data = ProjectData(root_folder=moreFoldersProjectPath)
+        self.assertEqual(my_project_data.productiveCodeNoFiles, 9, 'All files must be found')
+        self.assertEqual(len(my_project_data.productiveCodeFiles),
+                         my_project_data.productiveCodeNoFiles,
+                         'The number of files should correspond to the file list length')
+        self.assertEqual(sorted(my_project_data.productiveCodeFiles),
+                         sorted(["a/a.c","a/a.h","b/a.c","b/b.h","c/c.c","c/b.h",
+                                 "main.c","module.c","module.h"]),
+                         'All files should be found')
+    
+##    def test_ParseFolderPerformance(self):
+##        """
+##        Parsing 732112 file and return a list with all files ABSOLUTE path to the folder root
+##        Ran in 9.876s
+##        
+##        Parsing 754567 file and return a list with all files RELATIVE path to the folder root
+##        Ran in 5.632s
+##        
+##        """
+##        my_project_data = ProjectData(root_folder='/')
+##        self.assertEqual(my_project_data.productiveCodeNoFiles, 1, 'One file was found')
+        
+
+# EOF
--- tools/ProjectData.py
+++ tools/ProjectData.py
@@ -1,6 +1,37 @@
+import os
+
 class ProjectData(object):
     """
     a. Scans the project folder structure and updates the list with the
     productive code files.
     """
-    pass
+    def __init__(self,root_folder=None):
+        self.productiveCodeNoFiles = 0
+        self.rootFolderAbsPath = None
+        
+        if (root_folder and os.path.isdir(root_folder)):
+            self.rootFolderAbsPath = root_folder
+            self.productiveCodeNoFiles = self._getAllFiles()
+    
+    def _getAllFiles(self):
+        """
+        Parse the whole project folder and update the list with
+        all existing files (relative paths to the root folder).
+        The file list is sorted!
+        Return the number of found files
+        """
+        self.productiveCodeFiles = sorted(list(self._recursiveFolderScan()))
+        return len(self.productiveCodeFiles)
+    
+    def _recursiveFolderScan(self):
+        """
+        Recursively parse the project folder and return the found files
+        This function is a generator.
+        """
+        root_folder = self.rootFolderAbsPath+'/'
+        for dirpath, dirnames, filenames in os.walk(root_folder):
+            for filename in filenames:
+                yield os.path.join(dirpath.replace(root_folder, ''),filename)
+                
+                
+# EOF
